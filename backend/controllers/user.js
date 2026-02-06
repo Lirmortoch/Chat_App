5a7ace125bc22aa7ff4da3a30ac92826754eb7a7
Lirmortoch <dima2111323@gmail.com>
2026-02-06 21:40:49 +0300

    fix users controllers

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,65 +3,107 @@ const UserRouter = require('express').Router();
 
 const postgreSql = require('../db.js');
 
+const config = require('../utils/config.js');
+
 UserRouter.get('/', async (request, response) => {
-  const users = await postgreSql`
+  try {
+    const users = await postgreSql`
     SELECT public_id, email, name
     FROM users
   `;
-
-  if (!users) {
-    return response.status(404).end();
+  
+    response.json(users);
   }
-
-  response.json(users);
+  catch(error) {
+    console.log(error);
+    response.status(500).json({ message: 'Internal server error' })
+  }
 });
+UserRouter.get('/:public_id', async (request, response) => {
+  try {
+    const [ user ] = await postgreSql`
+      SELECT * FROM users WHERE public_id = ${request.params['public_id']}
+    `;
 
-UserRouter.get('/:id', async (request, response) => {
-  const [ user ] = await postgreSql`
-    SELECT * FROM users WHERE public_id = ${request.params['public_id']}
-  `;
+    if (!user) {
+      return response.status(404).json({ message: 'User not found' });
+    }
 
-  if (!user) {
-    return response.status(404).end();
+    response.json(user);
   }
-
-  response.json(user);
+  catch(error) {
+    console.log(error);
+    response.status(500).json({ message: `Internal server error` })
+  }
 });
 
 UserRouter.post('/', async (request, response) => {
-  const { name, username, password, email, phoneNumber, role } = request.params;
-  const saltRounds = 25;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+  try {
+    const { name, username, password, email, phoneNumber, role } = request.body;
 
-  const insertedUser = await postgreSql`
-    INSERT INTO users (
-      name,
-      username,
-      password_hash,
-      email,
-      phone_number,
-      created_at,
-      deleted,
-      restricted,
-      role
-    )
-    SELECT
-      ${name},
-      ${username},
-      ${passwordHash},
-      ${email},
-      ${phoneNumber},
-      now(),
-      false,
-      false,
-      ${role}
-  `;
+    if (!password || !email) {
+      return response.status(400).json({ message: 'Missing required fields' });
+    }
+    
+    const saltRounds = config.SALT_ROUNDS;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  if (!user) {
-    return response.status(404).end();
+    const insertedUser = await postgreSql`
+      INSERT INTO users (
+        name,
+        username,
+        password_hash,
+        email,
+        phone_number,
+        created_at,
+        deleted,
+        restricted,
+        role
+      )
+      VALUES (
+        ${name},
+        ${username},
+        ${passwordHash},
+        ${email},
+        ${phoneNumber},
+        now(),
+        false,
+        false,
+        ${role}
+      )
+      RETURNING public_id, email, name
+    `;
+
+    response.status(201).json(insertedUser);
   }
+  catch(error) {
+    console.log(error);
+    response.status(500).json({ message: `Internal server error` })
+  }
+});
 
-  response.json(user);
+UserRouter.put('/:public_id', async (request, response) => {
+  
+})
+
+UserRouter.delete('/:public_id', async (request, response) => {
+  try {
+    const deletedUser = await postgreSql`
+      DELETE from users 
+      WHERE public_id = ${request.params['public_id']}
+      RETURNING public_id
+    `;
+
+    if (deletedUser.length === 0) {
+      return response.status(404).json({ message: 'User not found' });
+    }
+
+    response.status(204).end();
+  }
+  catch(error) {
+    console.log(error);
+    response.status(500).json({ message: `Internal server error` })
+  }
 });
 
 module.exports = UserRouter;
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -6,10 +6,12 @@ const SERVER_PORT = process.env.SERVER_PORT;
 const POSTGRE_DB_URI = process.env.POSTGRE_DB_URI;
 const POSTGRE_DB_USERNAME = process.env.POSTGRE_DB_USERNAME;
 const POSTGRE_DB_PASSWORD = process.env.POSTGRE_DB_PASSWORD;
+const SALT_ROUNDS = process.env.SALT_ROUNDS;
 
 module.exports = { 
   SERVER_PORT, 
   POSTGRE_DB_URI, 
   POSTGRE_DB_USERNAME,
   POSTGRE_DB_PASSWORD,
+  SALT_ROUNDS,
 }
